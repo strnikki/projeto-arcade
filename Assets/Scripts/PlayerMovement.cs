91df6e7ff5f7e4a8db66aed40bc1bcc929f3dfca
Nicole Fanelli <nicolefanellib@gmail.com>
2021-03-21 06:27:15 -0300

    Create base project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed = 7f;
+    [SerializeField] float gravity = -9.8f;
+    [SerializeField] float jumpHeight = 3f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    private CharacterController controller;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        move = Vector3.ClampMagnitude(move, 1f);
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            // v = sqrt(h * -2 * g) ??
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Gravity Update
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
