041ffa9ebd0b84c7a0fa1ebb24941a2424852d6d
Nicole Fanelli <nicolefanellib@gmail.com>
2021-03-22 22:33:09 -0300

    Refactor code and fix dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 7f;
+    [SerializeField] float speed;
+    [SerializeField] float groundSpeed = 7f;
     [SerializeField] float gravity = -9.8f;
     [SerializeField] float jumpHeight = 3f;
-    [SerializeField] float dashDistance = 5f;
+    [SerializeField] float dashSpeed = 100f;
+    [SerializeField] float dashDuration = 0.01f;
+
 
     [SerializeField] Transform groundCheck;
     [SerializeField] float groundDistance = 0.4f;
@@ -17,38 +20,77 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 movePos;
     private bool isGrounded;
+    private bool isDashing;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        speed = groundSpeed;
     }
 
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if(!isDashing)
+            GetMovementInput();
+        Jump();
+        Dash();
+        
+        if(isDashing)
+            CheckBounds();
 
-        Vector3 inputs = Vector3.zero;
-        inputs.x = Input.GetAxis("Horizontal");
-        inputs.z = Input.GetAxis("Vertical");
-
-        movePos = transform.right * inputs.x + transform.forward * inputs.z;
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * gravity), ForceMode.VelocityChange);
-            //rb.velocity = New Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-        }
-        if (Input.GetButtonDown("Dash"))
-        {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime)));
-            rb.AddForce(dashVelocity, ForceMode.VelocityChange);
-        }
     }
 
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movePos * speed * Time.fixedDeltaTime);
+    }
+
+    private void GetMovementInput()
+    {
+        Vector3 inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.z = Input.GetAxis("Vertical");
+
+        movePos = transform.right * inputs.x + transform.forward * inputs.z;
+    }
+
+    private void Jump()
+    {
+         if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * gravity), ForceMode.VelocityChange);
+            //rb.velocity = New Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetButtonDown("Dash"))
+        {
+            isDashing = true;
+            rb.drag = 8f;
+            rb.AddForce(movePos.normalized * dashSpeed, ForceMode.VelocityChange);
+            StartCoroutine(DashTimer());
+        }
+    }
+
+    IEnumerator DashTimer()
+    {
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+        rb.drag = 1f;
+        rb.velocity = Vector3.zero;
+    }
+
+    private void CheckBounds()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, movePos, 8f, groundMask))
+        {
+            movePos = Vector3.zero;
+        }
     }
 }
